6f309af0b9159bf9a6c6bde5f54ca1ce01f8d942
Charlotte Payne <cltpayne@gmail.com>
2018-01-14 17:39:10 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,6 +1,7 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
 }
+var array = ["function", "string"]
 var newArray = ["element", ...array];
 
 function destructivelyAddElementToBeginningOfArray(array, element){
