fcc95fe126b161dc66369d5dfffaf327fd2a760b
Charlotte Payne <cltpayne@gmail.com>
2018-01-14 17:47:04 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -7,6 +7,7 @@ var newArray = ["element", ...array];
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+  array.unshift(element)
 }
 
 array.unshift("bolean")
