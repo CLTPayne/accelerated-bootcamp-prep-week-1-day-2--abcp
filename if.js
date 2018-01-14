540f57155c3e83d3c7e6843d2a35e7989ca9c62f
Charlotte Payne <cltpayne@gmail.com>
2018-01-14 18:47:50 +0000

    automatically backed up by learn

--- if.js
+++ if.js
@@ -4,11 +4,12 @@ if (pokemon == "Pikachu") {
   console.log("I choose you Pikachu!!")
 }
 
-function greaterThan10(number)
+function greaterThan10(number) {
   if (number > 10) {
   return number
 }
 else {
   console.log("Sorry! That number is less than 10.")
+}
 }
 
